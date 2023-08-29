bcc6f159c311a5760a4421d12cc36ff512f04756
aizatkmrdin <59000200+aizatkmrdin@users.noreply.github.com>
2023-08-30 05:27:02 +0800

    initial release

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static String baseUrl = 'https://dummyjson.com';
+  static String getProductsEndpoint = '/products';
+  static String searchProductEndpoints = '/products/search?q=';
+}
